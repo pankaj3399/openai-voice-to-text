8a5bf232029d42c4a4215485ee97b31a7a0416e8
soumik9 <soumik.ahammed.9@gmail.com>
2023-12-19 00:32:18 +0600

    banner animation

--- client/src/views/Home/componets/Banner.jsx
+++ client/src/views/Home/componets/Banner.jsx
@@ -1,28 +1,23 @@
 import { MdArrowRightAlt } from "react-icons/md";
 import '../styles/banner.css'
+import BannerHeadWrap from "../partials/Banner/BannerHeadWrap";
+import CourseInfoWrap from "../partials/Banner/CourseInfoWrap";
 
 const Banner = () => {
     return (
         <header data-aos="fade-down"
-            data-aos-duration="200" id="banner" data-w-id="2d04cc36-9470-def7-6d38-4376e157814d" className="hero-section">
+            data-aos-duration="1500" id="banner" data-w-id="2d04cc36-9470-def7-6d38-4376e157814d" className="hero-section">
             <div className="custom-container">
                 <div className="w-layout-grid banner-grid">
                     <div id="w-node-_2e26540b-db34-503c-dd8c-cc4db4113f74-cf4aff70" className="banner-content">
-                        <div className="banner-heading-wrapper">
-                            <h1 data-w-id="08274e5c-1d05-22f6-7e64-4b192141d4df" className="hero-title">Doorbreek De Administratieve Barrières 🚀</h1>
-                        </div>
+                        <BannerHeadWrap />
                         <p className="paragraph">Focus op zorg, niet op papierwerk.</p>
                         <div className="banner-button-wrapper">
                             <a href="#contact" data-w-id="1429b358-4c3e-fc75-0db3-8b729619ff15" className="primary-button w-inline-block">
                                 <div>Kom in Contact.</div>
                                 <div className="button-icon banner-animate-bounce"><MdArrowRightAlt /></div>
                             </a>
-                            <div className="course-info-wrapper">
-                                <div data-w-id="0e03af90-2cb5-e065-7d93-07e9ac6cdbe6" className="course-info-block">
-                                    <div className="numbers-info">20 min 🔥</div>
-                                    <div className="detail-info">Besparing per intake</div>
-                                </div>
-                            </div>
+                            <CourseInfoWrap />
                         </div>
                     </div>
                     <div className="banner-image-wrapper">
